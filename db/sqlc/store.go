e0d58b923b713eae319073f4a770390399cd14b4
Amirshox Muxitdinov <amirshoxmuxitdinov@gmail.com>
2022-09-03 14:27:07 +0300

    test

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -39,3 +39,10 @@ func (store *Store) execTx(ctx context.Context, fn func(queries *Queries) error)
 
 	return tx.Commit()
 }
+
+// TransferTxParams contains the input parameters of the transfer transaction
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
