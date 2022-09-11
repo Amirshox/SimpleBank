bdf9836e938fb9cf10ead8b04ad007fea655c9de
amirshokh <amirshoxmuxitdinov@gmail.com>
2022-09-12 01:33:39 +0500

    entry test

--- db/sqlc/entry_test.go
+++ db/sqlc/entry_test.go
@@ -34,17 +34,57 @@ func TestCreateEntry(t *testing.T) {
 }
 
 func TestGetEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
 
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
 }
 
 func TestUpdateEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
 
+	arg := UpdateEntryParams{
+		ID:     entry1.ID,
+		Amount: utils.RandomMoney(),
+	}
+
+	entry2, err := testQueries.UpdateEntry(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
 }
 
 func TestDeleteEntry(t *testing.T) {
+	entry1 := CreateRandomEntry(t)
+	err := testQueries.DeleteEntry(context.Background(), entry1.ID)
 
+	require.NoError(t, err)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry2)
 }
 
 func TestListEntry(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		CreateRandomEntry(t)
+	}
 
+	arg := ListEntryParams{
+		Limit:  5,
+		Offset: 5,
+	}
+
+	entries, err := testQueries.ListEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, entries, 5)
+
+	for _, account := range entries {
+		require.NotEmpty(t, account)
+	}
 }
